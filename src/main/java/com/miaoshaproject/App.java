0ea91f978debf6d643d13e046c88432e81d3a2f4
e <e>
2019-06-25 22:13:42 +0800

    秒杀项目1.0

---
+++ src/main/java/com/miaoshaproject/App.java
@@ -0,0 +1,14 @@
+package com.miaoshaproject;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableAutoConfiguration
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
